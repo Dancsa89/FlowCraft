4d065ddbf357e2f6cd232374ca5dd4e385cf4e43
Dancsa89 <dancsa89@gmail.com>
2018-12-30 15:02:21 +0100

    hero added

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import table.GameTable;
+
 public class Main {
     public static void main(String[] args) {
 
---
+++ src/table/GameTable.java
@@ -0,0 +1,52 @@
+package table;
+
+import species.Hero;
+
+public class GameTable {
+
+    private final int size = 40;
+    private GameTableCell[][] gameField;
+
+    public GameTable() {
+        gameField = new GameTableCell[size][size];
+        addHero(0, 39);
+        addHero(39, 0);
+        fillTable();
+    }
+
+    public void addHero(int first, int second) {
+        for (int i = 0; i < gameField.length; i++) {
+            for (int j = 0; j < gameField[i].length; j++) {
+                if (gameField[first][second] == null) {
+                    Hero newHero = new Hero("Human");
+                    gameField[first][second] = newHero;
+                    newHero.setOccupied(true);
+                    newHero.setHero(true);
+                    newHero.setValue(1);
+                }
+            }
+        }
+    }
+
+    public void fillTable() {
+        for (int i = 0; i < gameField.length; i++) {
+            for (int j = 0; j < gameField[i].length; j++) {
+                if (gameField[i][j] == null) {
+                    gameField[i][j] = new GameTableCell();
+                }
+            }
+        }
+    }
+
+    public String toString() {
+        String resultArea = "";
+
+        for (int i = 0; i < gameField.length; i++) {
+            for (int j = 0; j < gameField[i].length; j++) {
+                    resultArea += ' ' + gameField[i][j].toString();
+            }
+                resultArea += "\n";
+        }
+        return resultArea;
+    }
+}
